16f99cbf922c264197496fd5f88d490f17afe6f0
SzymonGajdowski <sz.gajdowski@gmail.com>
2020-08-15 13:51:06 +0200

    Add models.

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'title': 'Blog Post 1',
-        'author': 'Jhon jhonny',
-        'date_posted': '01-12-2019',
-        'content': "Lorem ipsum dolor sit amet, consectetur adipiscing eit,"   
-    },
-    {
-        'title': 'Blog Post 2',
-        'author': 'Diego Esperanza',
-        'date_posted': '14-03-2020',
-        'content': "consectetur adipiscing elit"
-    }
-]
 
 def home(request):
-    content = {
-        'posts': posts
+    context = {
+        'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html', content)
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
